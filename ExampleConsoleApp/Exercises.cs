e9965e9dea93eb1e96678b9ec4a372c580a392f4
Colt15 <billyeveryman19@gmail.com>
2017-01-18 21:28:52 -0500

    v1

---
+++ ExampleConsoleApp/Exercises.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleConsoleApp
+{
+    class Exercises
+    {
+        public int maxOfFive(int number1, int number2, int number3, int number4, int number5)
+        {
+        List<int> myNumbers = new List<int>();
+            myNumbers.Add(number1);
+            myNumbers.Add(number2);
+            myNumbers.Add(number3);
+            myNumbers.Add(number4);
+            myNumbers.Add(number5);
+
+            return myNumbers.Max();
+        }
+        public bool IsPalindrome(string palinvalue)
+        {
+            int min = 0;
+            int max = palinvalue.Length - 1;
+            while (true)
+            {
+                if (min > max)
+                {
+                    return true;
+                }
+                char a = palinvalue[min];
+                char b = palinvalue[max];
+                if (char.ToLower(a) != char.ToLower(b))
+                {
+                    return false;
+                }
+                min++;
+                max--;
+            }
+        }
+        public string findLongestWord(string wordvalue)
+        {
+
+        }
+    }
+}
---
+++ ExampleConsoleApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Exercises exercise = new Exercises(); //instantiate object from class
+
+            Console.WriteLine(exercise.maxOfFive(4, 4, 6, 7, 8));
+
+            string[] palinarray =
+            {
+            "radar",
+            "dog",
+            "cat",
+            "levels",
+            "dewed",
+            };
+            foreach (string palinvalue in palinarray)
+            {
+                Console.WriteLine("{0} = {1}", palinvalue, exercise.IsPalindrome(palinvalue));
+            }
+
+            string[] wordarray =
+            {
+                "Hello",
+                "Fantastic",
+                "Hat",
+                "Punch",
+            };
+            foreach (string wordvalue in wordarray)
+            {
+                Console.WriteLine()
+            };
+
+
+            Console.ReadKey();
+        }
+    }
+}
